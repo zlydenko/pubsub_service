f278436c2f0e734e777e597a3e02ae3f8e236778
zlydenko <anton.zlydenko@gmail.com>
2025-02-09 15:56:12 +0200

    dto for environment variables and default values

---
+++ src/config/config.dto.ts
@@ -0,0 +1,18 @@
+import { IsInt, IsOptional, IsString } from "class-validator";
+
+import { DEFAULT_CONFIG, IConfig } from "./config.constants";
+
+export class EnvironmentVariablesDto implements IConfig {
+  @IsString()
+  REDIS_HOST: string = DEFAULT_CONFIG.REDIS_HOST;
+
+  @IsInt()
+  REDIS_PORT: number = DEFAULT_CONFIG.REDIS_PORT;
+
+  @IsOptional()
+  @IsString()
+  NODE_ENV?: string = DEFAULT_CONFIG.NODE_ENV;
+
+  @IsInt()
+  PORT: number = DEFAULT_CONFIG.PORT;
+}
